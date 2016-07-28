d611015cd519d6914df54f246a4ab651297bac0f
p1ma <pierre.junges@orange.fr>
2016-07-28 11:38:17 +0200

    classes creation

--- core/src/p1ma/game/MyGame.java
+++ core/src/p1ma/game/MyGame.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+/**
+ * Created by p1ma on 28/07/16.
+ */
 public class MyGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
---
+++ core/src/p1ma/game/model/World.java
@@ -0,0 +1,7 @@
+package p1ma.game.model;
+
+/**
+ * Created by p1ma on 28/07/16.
+ */
+public class World {
+}
