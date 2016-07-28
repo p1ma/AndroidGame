90f8b2e32cbcbe96be07673a6b38c99bbca17c8f
p1ma <pierre.junges@orange.fr>
2016-07-28 11:14:52 +0200

    1rst commit

---
+++ android/src/p1ma/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package p1ma.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import p1ma.game.MyGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGame(), config);
+	}
+}
---
+++ desktop/src/p1ma/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package p1ma.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import p1ma.game.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
